aad951511b70d914aa60affa389075dd444fafe4
Sobirovibrohim277 <i.coder.prog@gmail.com>
2022-01-19 16:07:47 +0500

    test

--- lib/core/constants/const_colors.dart
+++ lib/core/constants/const_colors.dart
@@ -7,3 +7,4 @@ const Color disabled = Color(0xFFEFEFEF);
 const Color red = Color(0xFFFF4949);
 const Color green = Color(0xFF55B963);
 const Color darkGrey = Color(0xFF454545);
+const Color black = Color(0xFF000000);
