348e4abbebb3b1cc71b0050168b2f826c660564c
XondamirAVG <avazhonovhondamir@gmail.com>
2022-01-19 17:04:43 +0500

    animal_icons

--- lib/core/constants/asset_icons.dart
+++ lib/core/constants/asset_icons.dart
@@ -8,3 +8,10 @@ class AssetIcons {
   static const String minus = 'assets/icons/minus.svg';
   static const String plus = 'assets/icons/plus.svg';
 }
+
+class AnimalICons {
+  static const String chicken = 'assets/animals_icon/chicken.svg';
+  static const String cow = 'assets/animals_icon/cow.svg';
+  static const String hourse = 'assets/animals_icon/hourse.svg';
+  static const String sheep = 'assets/animals_icon/sheep.svg';
+}
