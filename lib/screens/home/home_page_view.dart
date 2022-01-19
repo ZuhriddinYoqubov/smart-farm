9ff5ac37d4adce3da52f7f93b3fb6470304c558e
XondamirAVG <avazhonovhondamir@gmail.com>
2022-01-19 16:48:24 +0500

    1648

--- lib/screens/home/home_page_view.dart
+++ lib/screens/home/home_page_view.dart
@@ -5,7 +5,7 @@ import 'package:smartfarm/widgets/my_text_bold.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({Key? key}) : super(key: key);
-//dsanasksnlja
+//dsanasksnljadsadsada
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
