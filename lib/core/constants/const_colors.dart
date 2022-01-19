3ce651f28ae5dcbd0d725498526d986a79bb8df0
Sobirovibrohim277 <i.coder.prog@gmail.com>
2022-01-19 15:01:08 +0500

    RAnglar klassdan chiqarildi

--- lib/core/constants/const_colors.dart
+++ lib/core/constants/const_colors.dart
@@ -1,10 +1,9 @@
 import 'dart:ui';
 
-class ConstColors {
-  static const Color primary = Color(0xFFFF8843);
-  static const Color lightGrey = Color(0xFFDDDDDD);
-  static const Color grey = Color(0xFF999999);
-  static const Color disabled = Color(0xFFEFEFEF);
-  static const Color red = Color(0xFFFF4949);
-  static const Color green = Color(0xFF55B963);
-}
+const Color primary = Color(0xFFFF8843);
+const Color lightGrey = Color(0xFFDDDDDD);
+const Color grey = Color(0xFF999999);
+const Color disabled = Color(0xFFEFEFEF);
+const Color red = Color(0xFFFF4949);
+const Color green = Color(0xFF55B963);
+const Color darkGrey = Color(0xFF454545);
