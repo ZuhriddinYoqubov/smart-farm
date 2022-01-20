d5bd7226a3e633a96ad78898878564737cec269d
XondamirAVG <avazhonovhondamir@gmail.com>
2022-01-20 21:02:55 +0500

    Update

--- lib/core/constants/my_colors.dart
+++ lib/core/constants/my_colors.dart
@@ -9,4 +9,5 @@ static const Color red = Color(0xFFFF4949);
 static const Color green = Color(0xFF55B963);
 static const Color darkGrey = Color(0xFF454545);
 static const Color white = Color(0xFfffffff);
+static const Color black = Color(0xFF000000);
 }
---
+++ lib/widgets/product_button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:smartfarm/core/constants/my_asset_icons.dart';
+import 'package:smartfarm/core/constants/my_colors.dart';
+import 'package:smartfarm/core/constants/size_config.dart';
+import 'package:smartfarm/widgets/my_text_bold.dart';
+
+class ProductButton extends StatelessWidget {
+  String? txt;
+  String? img;
+  ProductButton({
+    required this.img,
+    required this.txt,
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Container(
+        height: getUniqueH(100.0),
+        width: getUniqueW(140.0),
+        color: Colors.blueAccent,
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image.asset(
+              img!,
+              fit: BoxFit.cover,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                        height: getUniqueH(39.0),
+                        width: getUniqueW(90.23),
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Image.asset(
+                              ProductName.name,
+                              fit: BoxFit.cover,
+                            ),
+                            Center(
+                              child: MyTextBold(
+                                text: txt!,
+                                size: 14,
+                              ),
+                            ),
+                          ],
+                        )),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+}
