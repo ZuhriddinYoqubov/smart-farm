dc30083fd4367273b3a1c3a88816583e8d0a64b8
Sobirovibrohim277 <i.coder.prog@gmail.com>
2022-01-20 15:33:43 +0500

    update text

---
+++ lib/widgets/my_text_regular.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:smartfarm/core/components/size_config.dart';
+
+class MyTextRegular extends StatelessWidget {
+  final String text;
+  double? size;
+  int? maxLines;
+  Color? color;
+  TextAlign? textAlign;
+
+  MyTextRegular(
+      {required this.text,
+      this.textAlign,
+      this.color,
+      this.size = 14.0,
+      Key? key,
+      this.maxLines})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: textAlign,
+      maxLines: maxLines,
+      style: TextStyle(
+        fontSize: getUniqueH(size!),
+        color: color,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+}
