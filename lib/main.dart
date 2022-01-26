13d4e7a37ef2a1115b8dc642c49c0c366831203e
BahromjonPolat <itjunior.uz@gmail.com>
2022-01-26 14:20:44 +0500

    MyBorderRadius, AnimalButton tayyor

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Smart Farm',
       theme: getApplicationTheme(),
-      home:  SignUpView(),
+      home:  TestPage(),
     );
   }
 }
