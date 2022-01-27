9b13d32eaafce0b9473e442b5806488bdaab809e
Zuhriddin Yoqubov <zuhriddinyoqub@gmail.com>
2022-01-27 17:04:51 +0500

    on boarding iwlanmalari bor

--- lib/screens/on_boardin_page/on_boarding_page_cubit.dart
+++ lib/screens/on_boardin_page/on_boarding_page_cubit.dart
@@ -1,16 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'on_boarding_page_state.dart';
 
 class OnBoardingPageCubit extends Cubit<OnBoardingPageState> {
   OnBoardingPageCubit() : super(OnBoardingPageInitial());
-  int indicatorIndex = 0;
-  int indicatorLength = 3;
-  
+  int currentPage = 0;
+  int pagesLength = 3;
+  final PageController controller = PageController(
+    initialPage: 0,
 
-  changeIndicatorIndex(int value){
-    indicatorIndex = value;
+  );
+
+  changeCurrentPage(int value) {
+    //print(value);
+    currentPage = value;
+    emit(OnBoardingPageInitial());
+  }
+  incrementCount(){
+    debugPrint( "after:" + currentPage.toString());
+    if(currentPage != 2) {
+      currentPage ++;
+    }
+    debugPrint( "before:" + currentPage.toString());
     emit(OnBoardingPageInitial());
   }
 }
