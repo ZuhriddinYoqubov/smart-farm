d248cf8888e11fd7f316cdb9c37a32c054e70cb0
Sobirovibrohim277 <i.coder.prog@gmail.com>
2022-01-28 12:23:18 +0500

    product_grid_page, farm_grid_page_view, animals_grid_page_view, product_card_bottom   added

--- lib/screens/search/farm_detail_page/farm_detail_page_view.dart
+++ lib/screens/search/farm_detail_page/farm_detail_page_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:smartfarm/core/components/exporting_packages.dart';
-import 'package:smartfarm/screens/home/nutrition_history_page/nutrition_history_page_view.dart';
-import 'package:smartfarm/widgets/my_nutrion_history_item.dart';
 
 class FarmDetailPageView extends StatefulWidget {
   const FarmDetailPageView({Key? key}) : super(key: key);
---
+++ lib/screens/search/products_grid_page/products_grid_page.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:smartfarm/core/components/exporting_packages.dart';
+import 'package:smartfarm/widgets/product_card_bottom.dart';
+
+class ProductsGridPage extends StatefulWidget {
+  const ProductsGridPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductsGridPage> createState() => ProductsGridPageState();
+}
+
+class ProductsGridPageState extends State<ProductsGridPage>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+  @override
+  void initState() {
+    _tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: getUniqueH(126.0),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        MySizedBox(width: getUniqueW(21.0)),
+                        InkWell(
+                          onTap: () {},
+                          child: SvgPicture.asset(
+                            MyAssetIcons.vector,
+                          ),
+                        ),
+                        MySizedBox(width: getUniqueW(44.0)),
+                        MyTextBold(
+                          text: "Sut mahsulotlar",
+                          size: 20.0,
+                        ),
+                      ],
+                    ),
+                    MySizedBox(height: getUniqueH(20.0)),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0),
+                      child: tabBar(),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  card(
+                    "12000",
+                    "Qatiq",
+                    "img",
+                  ),
+                  MySizedBox(
+                    width: getUniqueW(20.0),
+                  ),
+                  card(
+                    "15000",
+                    "Sut",
+                    "img",
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget tabBar() {
+    return TabBar(
+      controller: _tabController,
+      indicator: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: MyColors.primary,
+      ),
+      indicatorColor: Colors.grey,
+      indicatorSize: TabBarIndicatorSize.tab,
+      indicatorWeight: 4.0,
+      labelColor: MyColors.grey,
+      unselectedLabelColor: MyColors.black,
+      tabs: [
+        Tab(
+          child: MyTextRegular(
+            text: "Barchasi",
+            size: 16.0,
+          ),
+        ),
+        Tab(
+          child: MyTextRegular(
+            text: "Mol suti",
+            size: 16.0,
+          ),
+        ),
+        Tab(
+          child: MyTextRegular(
+            text: "Qatiq",
+            size: 16.0,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget card(price, text, img) {
+    return Column(
+      children: [
+        ProductCardBottom(
+          price: price,
+          text: text,
+          img: img,
+        ),
+        MySizedBox(height: getUniqueH(20.0)),
+        ProductCardBottom(
+          price: price,
+          text: text,
+          img: img,
+        ),
+        MySizedBox(height: getUniqueH(20.0)),
+        ProductCardBottom(
+          price: price,
+          text: text,
+          img: img,
+        ),
+        MySizedBox(height: getUniqueH(20.0)),
+        ProductCardBottom(
+          price: price,
+          text: text,
+          img: img,
+        ),
+        MySizedBox(height: getUniqueH(60.0)),
+      ],
+    );
+  }
+}
